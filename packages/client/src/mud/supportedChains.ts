0ceaff8b67ba55516498459cfc9712530743654a
ivcained <86070833+ivcained@users.noreply.github.com>
2023-10-27 14:59:41 +0000

    feat: baseGoerli

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -15,10 +15,16 @@ import {
   latticeTestnet,
   MUDChain,
   mudFoundry,
+  baseGoerli,
 } from "@latticexyz/common/chains";
+
+
+
+<network>.rpcUrls.default.http = [ 'https://base-goerli.g.alchemy.com/v2/dQowHy06IvvE954gG75A9UpPkjhXwp3a' ]
+<network>.rpcUrls.default.webSocket = [ 'wss://base-goerli.g.alchemy.com/v2/dQowHy06IvvE954gG75A9UpPkjhXwp3a' ]
 
 /*
  * See https://mud.dev/tutorials/minimal/deploy#run-the-user-interface
  * for instructions on how to add networks.
  */
-export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet];
+export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet, baseGoerli];
